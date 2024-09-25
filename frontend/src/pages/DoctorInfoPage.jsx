fe035396551a63992fa3c97a5bf093e19b2e1c04
Ansh Kumar <140062016+Ansh8052@users.noreply.github.com>
2024-09-25 21:39:41 +0530

    Update DoctorInfoPage.jsx

--- frontend/src/pages/DoctorInfoPage.jsx
+++ frontend/src/pages/DoctorInfoPage.jsx
@@ -137,8 +137,8 @@ const DoctorInfo = () => {
               )}
             </div>
 
-{/*             {/* Display Images */}
-            <div>
+
+{/*             <div>
               <h3 className="text-2xl font-semibold mb-4">Proof Documents</h3>
               <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                 {doctor.establishmentProof && (
@@ -174,7 +174,7 @@ const DoctorInfo = () => {
                   </div>
                 )}
               </div>
-            </div> */}
+            </div>  */}
           </div>
         </div>
       )}
